9d08b0d94b9599dd55f2505e5ee768b879d2550d
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2023-04-11 23:17:54 +0200

    typo

--- build/wizard/components/AddressInput.tsx
+++ build/wizard/components/AddressInput.tsx
@@ -71,7 +71,7 @@ const AddressInput = ({ onFinish, setWithdrawalAddress }: Props) => {
         <>
             <div className="sm:col-span-4">
                 <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Enter address (and tripple check!)
+                    Enter address (and triple check!)
                 </label>
                 <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
